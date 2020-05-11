85a92b3469d5f89d9e0ea7aeff243a8ae4be2718
Mollie-S <olcha.olcha@gmail.com>
2020-05-11 10:17:50 +0200

    Added some button values.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,22 +7,23 @@ function Calculator() {
       <Button />
       <Button />
       <Button />
-      <Button />
+      <Button id='divide' value={'÷'}/>
       <Button id='seven' value={'7'}/>
+
       <Button id='eight' value={'8'}/>
       <Button id='nine' value={'9'}/>
-      <Button />
+      <Button id='multiply' value={'×'}/>
       <Button id='four' value={'4'}/>
       <Button id='five' value={'5'}/>
       <Button id='six' value={'6'}/>
-      <Button />
+      <Button id='substract' value={'-'}/>
       <Button id='one' value={'1'}/>
       <Button id='two' value={'2'}/>
       <Button id='three' value={'3'}/>
-      <Button />
+      <Button id='add' value={'+'}/>
       <Button id='zero' value={'0'}/>
       <Button />
-      <Button />
+      <Button id='decimal' value={'.'}/>
       <Button id='equals' value={'='}/>
 
       </div>
