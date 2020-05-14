7a5999f34f63bd7d1bcc39c91b845fb4acb9393f
Mollie-S <olcha.olcha@gmail.com>
2020-05-14 09:17:57 +0200

    Created  the object with math operations properties. Replaced the eval() function with the object methods.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Keypad from './Keypad';
 import Display from './Display'
 
+const mathOperations =
+{
+  'add': (num1, num2) => num1 + num2,
+  'subtract': (num1, num2) => num1 - num2,
+  'multiply': (num1, num2) => num1 * num2,
+  'divide': (num1, num2) => num1 / num2
+}
 
 class Calculator extends React.Component {
   state = {
@@ -14,39 +21,53 @@ class Calculator extends React.Component {
   handleClick = (e) => {
     const {value, id} = e.target;
     const {previousValue, currentValue, operator} = this.state;
-    
+
     if (currentValue === '0' && id !== 'decimal') {
       this.setState({currentValue: ''})
     } else if (currentValue === '0' && id === 'decimal') {
-      this.setState({currentValue: '0.'})
+      this.setState({currentValue: '0'})
     }
 
     if (id === 'clear') {
       this.setState({previousValue: '', currentValue: '0', operator: ''});
-    } else if (/\d/g.test(value) || value === '.' ) {
+    } else if (/\d/g.test(value) || id  === 'decimal' ) {
         this.setState(prevState =>
           ({currentValue: prevState.currentValue.concat(value)}));
-    } else if (id === 'add'
-            || id === 'subtract'
-            || id === 'multiply'
-            || id === 'divide') {
+    } else if (id in mathOperations) {
         this.setState(prevState => {
           if (currentValue !== '') {
             return {
               currentValue: '',
               previousValue: prevState.currentValue,
-              operator: value
+              operator: id
             }
           } else {
             return {
-              operator: value
+              operator: id
             }
           }
         })
     }
-    else if (value === '=') {
-         this.setState({currentValue: eval(previousValue + operator + currentValue).toString()})
+    else if (id === 'equals') {
+       if (previousValue !== '' && operator !== '' && currentValue !== '') {
+         this.setState(prevState => {
+          let num1 = parseFloat(prevState.previousValue);
+          let num2 = parseFloat(prevState.currentValue);
+          let result = mathOperations[prevState.operator](num1,num2);
+
+           return {currentValue: result.toString(),
+                   previousValue: prevState.currentValue}
+         })
+       } else if (previousValue === '' && operator === '') {
+         this.setState({currentValue: currentValue})
+       } else if (previousValue !== '' && operator !== '' && currentValue === '') {
+         this.setState(prevState =>
+            ({currentValue: prevState.previousValue,
+              previousValue: prevState.previousValue}))
+
        }
+
+    }
   }
 
   render () {
