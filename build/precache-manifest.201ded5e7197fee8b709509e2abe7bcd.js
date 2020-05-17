7f1952ac5bd3367658234d2ec94c494e3e4f5942
Mollie-S <olcha.olcha@gmail.com>
2020-05-17 10:18:27 +0200

    Added production build files

---
+++ build/precache-manifest.201ded5e7197fee8b709509e2abe7bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ede062e8c18d8299523a06a0d1eeace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5707952eb688e4449bce",
+    "url": "/static/css/main.0f52c253.chunk.css"
+  },
+  {
+    "revision": "37b04cb0690882241431",
+    "url": "/static/js/2.1ade971c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ade971c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5707952eb688e4449bce",
+    "url": "/static/js/main.97589c24.chunk.js"
+  },
+  {
+    "revision": "2b1966e609115edb2dc0",
+    "url": "/static/js/runtime-main.787cdf2b.js"
+  }
+]);
