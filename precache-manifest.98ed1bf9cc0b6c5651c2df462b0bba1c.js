32fbd42d08ec9bcc446c64ac5080c7849bdc52b2
Mollie-S <olcha.olcha@gmail.com>
2020-05-18 12:54:27 +0200

    Updates

---
+++ precache-manifest.98ed1bf9cc0b6c5651c2df462b0bba1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d182c5233e95ee8609d19be05f9ae6",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "5707952eb688e4449bce",
+    "url": "/simple-calculator/static/css/main.0f52c253.chunk.css"
+  },
+  {
+    "revision": "37b04cb0690882241431",
+    "url": "/simple-calculator/static/js/2.1ade971c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-calculator/static/js/2.1ade971c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5707952eb688e4449bce",
+    "url": "/simple-calculator/static/js/main.97589c24.chunk.js"
+  },
+  {
+    "revision": "178241c3723cc264f9fb",
+    "url": "/simple-calculator/static/js/runtime-main.8eb1867e.js"
+  }
+]);
