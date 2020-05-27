c3cc7538b77141471f626004613850d251219719
Mollie-S <olcha.olcha@gmail.com>
2020-05-27 13:50:14 +0200

    Fixed the Header text width so that it changes on vewport change.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,21 @@
-import React from 'react';
-
+import React from "react";
 
 function Header() {
   return (
     <div style={headerStyle}>
-      <h1 style={{textAlign: 'center'}}>Calculator</h1>
+      <h1 style={{ textAlign: "center", fontSize: "10vw" }}>Calculator</h1>
     </div>
   );
 }
 const headerStyle = {
-backgroundColor: '#696773',
-color: '#fff',
-width: '70%',
-padding: '0.5rem',
-// margin: '0 auto 2rem auto'
-margin: '0 auto',
-marginBottom: '2rem'
-}
-
+  backgroundColor: "#696773",
+  color: "#fff",
+  width: "70%",
+  padding: "0.5rem",
+  // margin: '0 auto 2rem auto'
+  margin: "0 auto",
+  marginBottom: "2rem",
+  maxWidth: "100%",
+};
 
 export default Header;
